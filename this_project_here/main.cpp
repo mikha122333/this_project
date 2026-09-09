abf9bde35b1b2348a9c6caba3f6baa25a9096011
mikha122333 <mawashimisha03@gmail.com>
2026-09-09 19:54:42 +0300

    added class triangle in main.h

--- this_project_here/main.cpp
+++ this_project_here/main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <cstdlib>
+#include "main.h"
 
 int main(){
-    double a,a1,s;
-    std::cout << "enter hight of triangle: ";
-    std::cin >> a;
-    while (a <= 0) {
-        system("cls");
-        std::cout << "wrong input, try again: ";
+    double a,a1,flag;
+    flag = 0;
+    Triangle res;
+    while (flag == 0) {
+        std::cout << "enter hight of triangle: ";
         std::cin >> a;
-    };
-    std::cout << "enter side of triangle: ";
-    std::cin >> a1;
-    while (a1 <= 0) {
-        system("cls");
-        std::cout << "wrong input, try again: ";
+        std::cout << "enter side of triangle: ";
         std::cin >> a1;
+        try {
+            res.hight(a);
+            res.side(a1);
+            flag = 1;
+        }
+        catch (const std::range_error& tmp) {
+            std::cout << "error, wrong input";
+            system("cls");
+        }
     }
-    s=0.5*a*a1;
-    std::cout<<"area = "<< s << std::endl;
+    std::cout<<"area = "<<res.find_square()  << std::endl;
     return 0;
 }
