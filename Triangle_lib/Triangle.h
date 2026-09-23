d5398e6d45f2f0b6d7dbffabca0286dc5c25a59d
mikha122333 <mawashimisha03@gmail.com>
2026-09-23 12:56:58 +0300

    part of triangle is now in cpp instead of h

--- Triangle_lib/Triangle.h
+++ Triangle_lib/Triangle.h
@@ -11,26 +11,3 @@ public:
 	void side(const double side);
 	void hight(const double gight);
 };
-Triangle::Triangle() {
-	_side = 1;
-	_hight = 1;
-}
-double Triangle::find_square() const noexcept {
-	return 0.5 * _hight * _side;
-}
-double Triangle::side()const noexcept {
-	return _side;
-}
-double Triangle::hight()const noexcept {
-	return _hight;
-}
-void Triangle::side(const double side) {
-	if (side <= 0)
-		throw std::range_error("too small side");
-	_side = side;
-}
-void Triangle::hight(const double hight) {
-	if (hight <= 0)
-		throw std::range_error("too small hight");
-	_hight = hight;
-}
